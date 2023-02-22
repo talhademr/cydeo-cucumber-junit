8abdcfe0c8ac2e6c1b71ab29cecffc3d2a100dee
Talha Demir <talha.demir06@gmail.com>
2023-02-23 02:29:33 +0300

    Initial commit

---
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -0,0 +1,20 @@
+package com.cydeo.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+            plugin = "html:target/cucumber-report.html",
+            features = "src/test/resources/features",
+            glue = "com/cydeo/step_definitions",
+            dryRun = false,
+            tags = "@Regression"
+
+    )
+    public class CukesRunner {
+    }
+
+
